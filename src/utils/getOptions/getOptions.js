cad407c69e9fa0d0c02af315bfc312d84d87f049
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-25 11:04:34 -0500

    getOptions for menu added

---
+++ src/utils/getOptions/getOptions.js
@@ -0,0 +1,19 @@
+const getOptions = () => [
+  {
+    id: '1',
+    name: 'Inicio',
+    pathName: 'inicio'
+  },
+  {
+    id: '2',
+    name: 'Vivair',
+    pathName: 'vivair'
+  },
+  {
+    id: '3',
+    name: 'Avianca',
+    pathName: 'avianca'
+  }
+]
+
+export default getOptions
