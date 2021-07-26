95351740338fba7e95494b729c852ad95c1440f2
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-26 11:15:52 -0500

    VHanging the router wrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Header from './components/Header/Header'
 import 'semantic-ui-css/semantic.min.css'
 import MenuComponent from './components/Menu/Menu'
 import AppRouter from './router/AppRouter'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <MenuComponent />
-      <AppRouter />
+      <Router>
+        <Header />
+        <MenuComponent />
+        <AppRouter />
+        <Footer />
+      </Router>
     </div>
   )
 }
--- src/router/AppRouter/AppRouter.jsx
+++ src/router/AppRouter/AppRouter.jsx
@@ -1,17 +1,15 @@
 import React from 'react'
-import { Route, Switch, Redirect, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import AirLine from '../../layouts/AirLine'
 import Home from '../../layouts/Home'
 
 const AppRouter = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/home" component={Home} />
-      <Route exact path="/avianca" component={AirLine} />
-      <Route exact path="/vivair" component={AirLine} />
-      <Redirect to="/home" />
-    </Switch>
-  </Router>
+  <Switch>
+    <Route exact path="/home" component={Home} />
+    <Route exact path="/avianca" component={AirLine} />
+    <Route exact path="/vivair" component={AirLine} />
+    <Redirect to="/home" />
+  </Switch>
 )
 
 export default AppRouter
