5969a3a67dfd1ccdd87bf092c758c28fc9fdf267
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-26 11:16:45 -0500

    Link Menu to each layout path

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
 import { Menu } from 'semantic-ui-react'
 import getOptions from '../../utils/getOptions'
 import './Menu.scss'
@@ -6,7 +7,11 @@ import './Menu.scss'
 const MenuComponent = () => {
   const [activeItem, setActiveItem] = useState('inicio')
   const options = getOptions()
-  const handleItemClick = (e, { name }) => setActiveItem(name)
+  const history = useHistory()
+  const handleItemClick = (e, { name }) => {
+    setActiveItem(name)
+    history.push(`/${name}`)
+  }
 
   return (
     <Menu color="black" inverted widths={3} className="menu">
