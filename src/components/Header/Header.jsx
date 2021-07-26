6f7a453b7a86d052745f803c39f29dbd4e0befd5
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-26 15:01:01 -0500

    Adding link to hom from icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import { GiCrossedAirFlows } from 'react-icons/gi'
+import { Link } from 'react-router-dom'
 import './Header.scss'
 
 const Header = () => (
   <header className="header">
-    <div className="header__logo">
-      <GiCrossedAirFlows />
-      <div>
-        <h1>Travel Fly</h1>
+    <Link to="/home">
+      <div className="header__logo">
+        <GiCrossedAirFlows />
+        <div>
+          <h1>Travel Fly</h1>
+        </div>
       </div>
-    </div>
+    </Link>
     <div />
     <div>
       <h2 className="header__description">Las Mejores Aerolineas a los Mejores Precios</h2>
